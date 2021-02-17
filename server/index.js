cab4ed0a1adc0256c22f3ede3081e9069aeb477f
thejennify <60758016+thejennify@users.noreply.github.com>
2021-02-16 20:52:34 -0600

    added redis prod url

--- server/index.js
+++ server/index.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 3000;
 
 //REDIS CONFIG
 const redis = require("redis");
-const client = redis.createClient({
-  port: process.env.REDISCLOUD,
-});
+const client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
 
 client.on('error', function(err) {
   console.log('Redis error: ' + err);
