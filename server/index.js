e422b77e02a653fd7d518cf8c249cb29ae913691
thejennify <60758016+thejennify@users.noreply.github.com>
2021-02-16 20:31:38 -0600

    added env for redis

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 //REDIS CONFIG
 const redis = require("redis");
 const client = redis.createClient({
-  
+  port: process.env.REDISCLOUD,
 });
 //allows asynchronous methods when using redis
 const { promisify } = require("util");
